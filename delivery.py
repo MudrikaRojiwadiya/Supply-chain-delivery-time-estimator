227a2455ce730e02cb6be250b450dc4d476d62f4
MudrikaRojiwadiya <mudrikarojiwadiya@gmail.com>
2026-05-27 23:02:45 +0530

    Updated UI improvements

--- delivery.py
+++ delivery.py
@@ -14,22 +14,49 @@ data['source_city'] = data['source_name'].str.split('_').str[0]
 data['destination_city'] = data['destination_name'].str.split('_').str[0]
 
 
-st.title("Delivery Time Prediction")
-st.subheader("Enter the details to predict the delivery time")
+st.markdown("""
+<h1 style='text-align:center;
+color:#2E8B57;'>
+📦 Shipment Delivery Time Predictor
+</h1>
+
+<p style='text-align:center;
+font-size:20px;
+color:gray;'>
+Predict shipment arrival time using Machine Learning & Route Intelligence
+</p>
+
+<hr>
+""", unsafe_allow_html=True)
+
+st.sidebar.title("📦 About Project")
+
+st.sidebar.info("""
+This ML-based application predicts
+shipment delivery arrival using:
+
+✔ Random Forest
+✔ OSRM Routing
+✔ Streamlit
+✔ Geopy
+""")
 
 route_type = st.selectbox("Select route_type", data["route_type"].unique())
 
 
-source_city = st.selectbox(
-    "select your source city",
-    data['source_city'].str.split('_').str[0].unique()
-)
+col1, col2 = st.columns(2)
 
-destination_city = st.selectbox(
-    "select your destination city",
-    data['destination_name'].str.split('_').str[0].unique()
-)
+with col1:
+    source_city = st.selectbox(
+        "📍 Select Source City",
+        data['source_city'].unique()
+    )
 
+with col2:
+    destination_city = st.selectbox(
+        "🎯 Select Destination City",
+        data['destination_city'].unique()
+    )
 geolocator = Nominatim(user_agent="delivery_app")
 
 try:
@@ -76,8 +103,19 @@ osrm_distance, osrm_time = get_osrm_data(
 )
 
 # SHOW OUTPUT
-st.write("OSRM Distance:", round(osrm_distance, 2), "KM")
-st.write("OSRM Time:", round(osrm_time, 2), "Minutes")
+col3, col4 = st.columns(2)
+
+with col3:
+    st.metric(
+        "🛣 Distance",
+        f"{round(osrm_distance,2)} KM"
+    )
+
+with col4:
+    st.metric(
+        "⏱ Travel Time",
+        f"{round(osrm_time,2)} Minutes"
+    )
 
 # THEN groupby
 source_dict_center = data.groupby('source_city')['source_center'].first().to_dict()
@@ -87,16 +125,33 @@ destination_dict_center = data.groupby('destination_city')['destination_center']
 source_center = source_dict_center[source_city]
 destination_center = destination_dict_center[destination_city]
 
-source_center = st.write("Selected Source Center:", source_center)
-destination_center = st.write("Selected Destination Center:", destination_center)
+col5, col6 = st.columns(2)
 
-order_datetime = st.datetime_input(
-    "Select Order Date & Time"
-)
-dispatch_datetime = st.datetime_input(
-    "Select Dispatch Date & Time"
-)
+with col5:
+    st.info(f"""
+    📍 Source Center
 
+    {source_center}
+    """)
+
+with col6:
+    st.info(f"""
+    🎯 Destination Center
+
+    {destination_center}
+    """)
+
+col7, col8 = st.columns(2)
+
+with col7:
+    order_datetime = st.datetime_input(
+        "📅 Order Date & Time"
+    )
+
+with col8:
+    dispatch_datetime = st.datetime_input(
+        "🚚 Dispatch Date & Time"
+    )
 od_hour = dispatch_datetime.hour
 trip_hour = order_datetime.hour
 
@@ -132,17 +187,17 @@ traffic_map = {
 
 data['traffic_level'] = data['traffic_level'].map(traffic_map)
 
-if st.button("Predict Delivery Time"):   
+if st.button("🚀 Predict Delivery Time", use_container_width=True): 
     input_data = pd.DataFrame({
-        'source_center': [source_center],
-        'destination_center': [destination_center],
-        'route_type': [route_type],
-        'osrm_time': [osrm_time],
-        'osrm_distance': [osrm_distance],
-        'trip_hour': [trip_hour],
-        'od_hour': [od_hour],
-        'route_frequency': [route_frequency],
-        'traffic_level': [traffic_map[traffic_level(od_hour)]]
+    'source_center': [source_center],
+    'destination_center': [destination_center],
+    'route_type': [route_type],
+    'osrm_time': [osrm_time],
+    'osrm_distance': [osrm_distance],
+    'trip_hour': [trip_hour],
+    'od_hour': [od_hour],
+    'route_frequency': [route_frequency],
+    'traffic_level': [traffic_map[traffic_level(od_hour)]]
     })
     
     prediction = pipe.predict(input_data)[0]
